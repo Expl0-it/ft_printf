e80c17a5d8a830db8850b04f3b35f2288c67d1d4
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-05-13 18:10:52 +0200

    added @bug as precision doesnt yet work as intended
    probably problem with precision when NULL is given in %s or %p (only
    writing part of the word, not all of it
    )

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 19:07:32 by mamichal          #+#    #+#             */
-/*   Updated: 2024/04/24 14:53:35 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/05/13 18:09:32 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,16 +121,16 @@ typedef struct s_data
 /*
 * @brief Format and print data to stdout
 *
-• %c Prints a single character.
-• %s Prints a string (as defined by the common C convention).
-• %p The void * pointer argument has to be printed in hexadecimal format.
-• %d Prints a decimal (base 10) number.
-• %i Prints an integer in base 10.
-• %u Prints an unsigned decimal (base 10) number.
-• %x Prints a number in hexadecimal (base 16) lowercase format.
-• %X Prints a number in hexadecimal (base 16) uppercase format.
-• %% Prints a percent sign.
-*
+* %c Prints a single character.
+* %s Prints a string (as defined by the common C convention).
+* %p The void * pointer argument has to be printed in hexadecimal format.
+* %d Prints a decimal (base 10) number.
+* %i Prints an integer in base 10.
+* %u Prints an unsigned decimal (base 10) number.
+* %x Prints a number in hexadecimal (base 16) lowercase format.
+* %X Prints a number in hexadecimal (base 16) uppercase format.
+* %% Prints a percent sign.
+* @bug precision doesn't work correctly
 */
 int		ft_printf(const char *format, ...);
 
